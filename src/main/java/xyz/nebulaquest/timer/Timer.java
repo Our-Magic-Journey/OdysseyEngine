3f5f02aa6a320938e92aaba5dddc4bcfb24181f2
Amon De Shir <amonsir29@gmail.com>
2023-11-17 21:22:17 +0100

    Create timer

---
+++ src/main/java/xyz/nebulaquest/timer/Timer.java
@@ -0,0 +1,60 @@
+package xyz.nebulaquest.timer;
+
+import xyz.nebulaquest.event.Event;
+import xyz.nebulaquest.event.EventGetter;
+import xyz.nebulaquest.update.Updatable;
+
+public class Timer implements Updatable {
+  private long timeLeft;
+  private long waitTime;
+  private boolean paused;
+  private boolean autoRepeat;
+  private Event timeoutEvent;
+
+  public Timer(long waitTime) {
+    this(waitTime, false);
+  }
+
+  public Timer(long waitTime, boolean autoRepeat) {
+    this.waitTime = waitTime;
+    this.timeLeft = waitTime;
+    this.timeoutEvent = new Event();
+  }
+
+  public EventGetter onTimeout() {
+    return timeoutEvent;
+  }
+
+  public void reset() {
+    timeLeft = waitTime;
+    paused = false;
+  }
+
+  public void pause() {
+    paused = true;
+  }
+
+  public void resume() {
+    paused = false;
+  }
+
+  @Override
+  public void update(long deltaTime) {
+    if (paused) {
+      return;
+    }
+
+    timeLeft = Math.max(0, timeLeft - deltaTime);
+
+    if (timeLeft == 0) {
+      timeoutEvent.emit();
+
+      if (autoRepeat) {
+        this.reset();
+      }
+      else {
+        this.pause();
+      }
+    }
+  }
+}
