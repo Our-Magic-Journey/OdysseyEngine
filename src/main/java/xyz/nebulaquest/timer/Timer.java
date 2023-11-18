401cb1978dec458f3e7f40be5752e1e7c3cd30df
Amon De Shir <amonsir29@gmail.com>
2023-11-18 17:39:29 +0100

    Document the Timer class

--- src/main/java/xyz/nebulaquest/timer/Timer.java
+++ src/main/java/xyz/nebulaquest/timer/Timer.java
@@ -4,6 +4,13 @@ import xyz.nebulaquest.event.Event;
 import xyz.nebulaquest.event.EventGetter;
 import xyz.nebulaquest.update.Updatable;
 
+/**
+ * A simple countdown timer. It counts down a specified interval and emits an event on reaching 0.
+ * <p>Can be set to repeat mode or be paused.</p>
+ * 
+ * <b>Events</b>
+ * <p>{@code onTimeout} emitted when the time set in {@code waitTime} parameter elapses.</p>
+ */
 public class Timer implements Updatable {
   private long timeLeft;
   private long waitTime;
@@ -11,34 +18,77 @@ public class Timer implements Updatable {
   private boolean autoRepeat;
   private Event timeoutEvent;
 
+  /**
+   * Constructs a timer with the specified waiting time and disabled auto-repeat.
+   * The timer is initially stopped. Use {@code resume()} to start it.   
+   * 
+   * @param waitTime The waiting time in milliseconds.
+   *  
+   */
   public Timer(long waitTime) {
     this(waitTime, false);
   }
 
+  /**
+   * Constructs a timer with the specified waiting time and optional auto-repetition.
+   * The timer is initially stopped. Use {@code resume()} to start it.   
+   * 
+   * @param waitTime The waiting time in milliseconds.
+   * @param autoRepeat If true, the timer will stop when reaching 0. If false, it will restart.
+   */
   public Timer(long waitTime, boolean autoRepeat) {
     this.waitTime = waitTime;
     this.timeLeft = waitTime;
-    this.timeoutEvent = new Event();
+    this.autoRepeat = autoRepeat;
     this.paused = true;
+    this.timeoutEvent = new Event();
   }
 
-  public EventGetter onTimeout() {
+  /**
+   * Getter to the timeout event that will be emitted when the time set in {@code waitTime} parameter elapses.
+   * 
+   * <p>Use {@code onTimeout().subscribe()} to add callbacks and {@code onTimeout().unsubscribe()} to remove them.</p>
+   *
+   * @return {@code EventGetter} a getter for the timeout event that allows subscribing or unsubscribing it
+   */
+   public EventGetter onTimeout() {
     return timeoutEvent;
   }
 
+  /**
+   * Resets the timer, setting the remaining time to the original waiting time.
+   * It also unpauses the timer if it was paused.
+   */
   public void reset() {
     timeLeft = waitTime;
     paused = false;
   }
 
+  /**
+   * Pauses the timer.
+   */
   public void pause() {
     paused = true;
   }
 
+  /**
+   * Resumes the timer if it was paused.
+   * 
+   * <p><b>NOTE:</b> If the timer was paused because it has reached the specified time, using resume will not reset the timer; 
+   * in such cases, use {@code reset()} to start the timer again.</p>
+   */
   public void resume() {
     paused = false;
   }
 
+  /**
+   * Updates the timer's state within the game's update cycle.
+   * Call this method to make the timer functional.
+   * 
+   * <p><b>NOTE:</b> If the timer is paused, the update will have no effect until it is resumed.</p>
+   * 
+   * @param deltaTime The time by which to update the timer in milliseconds.
+   */
   @Override
   public void update(long deltaTime) {
     if (paused) {
