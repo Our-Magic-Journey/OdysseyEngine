b9b688de1d5afa1acc69e969cca61533d10a7938
Dominikaninn <dominikprabucki.2002@gmail.com>
2023-11-18 14:42:36 +0100

    Add return to main menu functionality
    Introduce a return to the main menu feature on the CreditsScreen:
    - Added a dedicated button for returning to the main menu.
    - Implemented a timer that automatically returns users to the main menu
      after 10 seconds.
    
    Additional Changes:
    - Encapsulated variables in the CreditsScreen.java file for improved
      code maintainability.

--- src/main/java/xyz/nebulaquest/screen/CreditsScreen.java
+++ src/main/java/xyz/nebulaquest/screen/CreditsScreen.java
@@ -4,15 +4,19 @@ import java.awt.Color;
 
 import xyz.nebulaquest.input.InputManager;
 import xyz.nebulaquest.renderer.Renderer;
+import xyz.nebulaquest.timer.Timer;
 import xyz.nebulaquest.ui.Image;
 import xyz.nebulaquest.ui.Text;
+import xyz.nebulaquest.ui.TextButton;
 
 
 public class CreditsScreen extends Screen {
-  public Image background;
-  public Text title;
-  public Text autor1;
-  public Text autor2;
+  private Image background;
+  private Text title;
+  private Text autor1;
+  private Text autor2;
+  private TextButton returnButton;
+  private Timer returnTimer;
 
   public CreditsScreen(InputManager inputManager, ScreenManager screenManager) {
     super(inputManager, screenManager);
@@ -22,17 +26,33 @@ public class CreditsScreen extends Screen {
   public void load() {
     background = new Image("/background.png", 0, 0);
     title = new Text("Nebula Quest", 280, 100, "/fonts/NewRocker.ttf", new Color(0xff7b00), 70);
-    autor1 = new Text("Dominik", 280, 250, "/fonts/Fervojo-Regular.otf", new Color(0xff7b00), 50);
-    autor2 = new Text("Dominik", 280, 310, "/fonts/Fervojo-Regular.otf", new Color(0xff7b00), 50);
+    autor1 = new Text("Dominik1", 280, 250, "/fonts/Fervojo-Regular.otf", new Color(0xff7b00), 50);
+    autor2 = new Text("Dominik2", 280, 310, "/fonts/Fervojo-Regular.otf", new Color(0xff7b00), 50);
+    returnButton = new TextButton("Return", 150, 410, "/fonts/Fervojo-Regular.otf", new Color(0xff7b00), 50, inputManager);
+    returnTimer = new Timer(10000);
+    
+    returnButton.onClick().subscribe(()->this.backToMenu());
+    returnTimer.onTimeout().subscribe(this::backToMenu);
+
+    returnTimer.resume();
+  }
+
+  private void backToMenu() {
+    this.screenManager.change("menu");
   }
 
 
+  @Override
+  public void unload() {
+    returnTimer.onTimeout().unsubscribe(this::backToMenu);
+    returnButton.dispatch(inputManager);
+  }
 
   @Override
-  public void unload() {}
-
-  @Override
-  public void update(long deltaTime) {}
+  public void update(long deltaTime) {
+    returnButton.update(deltaTime);
+    returnTimer.update(deltaTime);
+  }
 
   @Override
   public void draw(Renderer renderer) {
@@ -40,6 +60,7 @@ public class CreditsScreen extends Screen {
     renderer.draw(title);
     renderer.draw(autor1);
     renderer.draw(autor2);
+    renderer.draw(returnButton);
 
   }
 
