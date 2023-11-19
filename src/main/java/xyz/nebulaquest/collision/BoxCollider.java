a93bcffc96a6ba8096cedce14931b5f3ca963777
Amon De Shir <amonsir29@gmail.com>
2023-11-19 03:07:04 +0100

    Document the BoxCollider class

--- src/main/java/xyz/nebulaquest/collision/BoxCollider.java
+++ src/main/java/xyz/nebulaquest/collision/BoxCollider.java
@@ -1,11 +1,22 @@
 package xyz.nebulaquest.collision;
 
+/**
+ * Rectangular box collider used for collision detection.
+ */
 public class BoxCollider {
   private int x;
   private int y;
   private int width;
   private int height;
 
+  /**
+   * Constructs a new BoxCollider with the specified position and dimensions.
+   *
+   * @param x      The x-coordinate of the top-left corner of the box.
+   * @param y      The y-coordinate of the top-left corner of the box.
+   * @param width  The width of the box.
+   * @param height The height of the box.
+   */
   public BoxCollider(int x, int y, int width, int height) {
     this.x = x;
     this.y = y;
@@ -13,6 +24,13 @@ public class BoxCollider {
     this.height = height;
   }
 
+  /**
+   * Checks if the specified point is contained within the box collider.
+   *
+   * @param pX The x-coordinate of the point.
+   * @param pY The y-coordinate of the point.
+   * @return {@code true} if the point is inside the box, {@code false} otherwise.
+   */
   public boolean containsPoint(int pX, int pY) {
     return pX >= this.x
         && pY >= this.y
@@ -20,6 +38,12 @@ public class BoxCollider {
         && pY <= this.y + this.height;
   }
 
+  /**
+   * Checks if this box collider collides with another box collider.
+   *
+   * @param box The other box collider to check for collision.
+   * @return {@code true} if there is a collision, {@code false} otherwise.
+   */
   public boolean collides(BoxCollider box) {
     return this.x <= box.x + box.width 
         && this.y <= box.y + box.height 
