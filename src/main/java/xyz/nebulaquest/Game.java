5f388a7db21e91f2f482b8714ca1a28acff4754a
Amon De Shir <amonsir29@gmail.com>
2023-11-19 07:16:11 +0100

    Document Game

--- src/main/java/xyz/nebulaquest/Game.java
+++ src/main/java/xyz/nebulaquest/Game.java
@@ -6,6 +6,24 @@ import xyz.nebulaquest.renderer.Window;
 import xyz.nebulaquest.renderer.Canvas;
 import xyz.nebulaquest.screen.ScreenManager;
 
+/**
+ * The main class of the NebulaQuest game.
+ * It manages the game loop, input, rendering, and screen transitions.
+ *
+ * <p>The game initializes components such as the window, canvas, renderer,
+ * input manager, and screen manager. It runs on a separate thread to keep
+ * the UI responsive and handles the main game loop, updating screens,
+ * and drawing on the canvas.</p>
+ *
+ * <p>The main method creates an instance of the game, triggering the setup
+ * and starting the game loop when the canvas is ready.</p>
+ *
+ * @see Window
+ * @see Canvas
+ * @see Renderer
+ * @see InputManager
+ * @see ScreenManager
+ */
 public class Game implements Runnable {
   private Window window;
   private Canvas canvas;
@@ -16,6 +34,9 @@ public class Game implements Runnable {
   private boolean running;
   private long lastFrame;
 
+  /**
+  * The main method that creates an instance of the game.
+  */
   public static void main(String[] args) {
     new Game();
   }
@@ -33,11 +54,18 @@ public class Game implements Runnable {
     window.attachCanvas(canvas);
   }
 
+  /**
+   * Initiates the game setup when the canvas is ready.
+   * Observes the canvas for input events and starts the game loop.
+   */
   private void start() {
     inputManager.observe(canvas);
     initThread();
   }
 
+  /**
+   * Initializes the game thread if it's not already running.
+   */
   private void initThread() {
     if (thread == null) {
       thread = new Thread(this);
@@ -45,10 +73,18 @@ public class Game implements Runnable {
     }
   }
 
+  /**
+   * Closes the game by setting the running flag to false.
+   */
   public void close() {
     this.running = false;
   }
 
+  /**
+   * The main game loop that runs on a separate thread.
+   * It continuously updates screens and renders on the canvas.
+   * Exits the loop when the game is set to close.
+   */
   @Override
   public void run() {
     this.running = true;
@@ -56,6 +92,10 @@ public class Game implements Runnable {
     this.gameLoop();
   }
 
+  /**
+   * The core game loop that updates screens and renders on the canvas.
+   * Calculates the time elapsed between frames for smooth updates.
+   */
   public void gameLoop() {
     while (running) {
       long deltaTime = calculateLastFrameDuration();
@@ -68,6 +108,12 @@ public class Game implements Runnable {
     window.close();
   }
 
+  /**
+   * Calculates the duration of the last frame in milliseconds.
+   * Updates the last frame timestamp for the next calculation.
+   *
+   * @return The duration of the last frame in milliseconds.
+   */
   private long calculateLastFrameDuration() {
     long current = System.currentTimeMillis();
     long duration = current - lastFrame;
